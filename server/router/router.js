95b7f41a9de29806eef83a30fccd4dca2fc07ccc
jungwon <gardenlike.p@gmail.com>
2022-10-10 18:20:32 +0200

    feat: add artworks router

--- server/router/router.js
+++ server/router/router.js
@@ -4,9 +4,14 @@ const { Router } = require('express');
 const router = Router();
 
 const exhibitionController = require('../controller/exhibition');
+const artworkController = require('../controller/artwork');
 
 router.get('/exhibitions', exhibitionController.getExhibitions);
 router.post('/exhibitions', exhibitionController.postExhibition);
 router.delete('/exhibitions/:id', exhibitionController.deleteExhibition);
+
+router.get('/artworks', artworkController.getArtworks);
+router.post('/artworks', artworkController.postArtwork);
+router.delete('/artworks', artworkController.deleteArtwork);
 
 module.exports = router;
