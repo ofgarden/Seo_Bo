481fad468582a4f9a917553ae802b886fe8bf272
jungwon <gardenlike.p@gmail.com>
2022-10-13 18:30:16 +0200

    feat: add description text

---
+++ client/src/assets/description.ts
@@ -0,0 +1,31 @@
+export const lateDescription =
+  'Embarking on his late period with an approach that hs become known as the late-écriture, ' +
+  'Park Seo-Bo began using tools such as sticks and rulers in place of his fingers to scratch and ' +
+  'rub Korean hanji paper, creating long and pronounced lines and furrow-like planes on the canvas ' +
+  'by applying pressure at regular intervals. Returning to black-and-white canvases with this transformation ' +
+  'in his artistic approach, he became even more resolute in his painting convictions in the early 2000s with ' +
+  'an experience of powerful healing within nature after witnessing landscapes at the height of the autumn foliage. ' +
+  'His endless passion for his work led him to the new works produced for this exhibition, which combine ' +
+  'his late-écriture and the early-écriture approaches.';
+ 
+  export const midDescription = 
+  'The 1980s represented the mid-écriture known for the zigzag period of Park Seo-Bo’s work ' +
+  'would undergo another change in 1982 with his rediscovery of dakjongi(mulberry paper). ' +
+  'Making maximum use of the material qualities of traditional Korean paper and emphasising performativity ' +
+  'with its repeated actions of rubbing, scraping, and pushing before the paint on the paper has dried, this work has been referred to as his mid-écriture ' +
+  'because of the pronounced quality of free directionality with the movements of the hands. ' +
+  'To restore a pictorial quality to the innumerable strokes, the artist also made attempts to capture the colours of nature ' +
+  'with infusions of mugwort and tobacco into Korean hang paper, while creating a sense of surging background colour ' +
+  'with the application of black over a primary colour base. ' +
+  'The intensive quality of his work during this period, which had to be completed before the paper dries, ' +
+  'led the artist to shift increasingly away from an approach of canvas segmentation toward his methods of the late-écriture period.';
+
+  export const earlyDescription = 
+   'Park Seo-Bo sought to produce works that incorporated tradition and adopted elements of Western art without being mere imitations. ' +
+   'Inspired by the unschooled handwriting of his son, his early-écriture approach was employed not out of a sense of having to create something new, ' +
+   'as with his Primordialis series and its works of outpouring emotion, but to produce paintings of ‘emptying’, which started from a place of resignation and abandonment. ' +
+   'For his early-écriture works, the artist applied a white base to the canvas and added innumerable pencil marks before the paint dried. ' +
+   'This works-in which materiality, spirituality, and the artist’s actions reach a place of unity ' +
+   'in the performative process-have been described by the artist as a ‘tool for self-cultivation.’';
+
+   // Park Seo-Bo: The Untiring Endeavorer. 18 May - 01 Sep 2019, National Museum of Modern and Contemporary Art, Korea.
