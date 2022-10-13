2c70096ab22b276444bd732e943d82b637f81f46
jungwon <gardenlike.p@gmail.com>
2022-10-13 18:30:49 +0200

    feat: add description

--- client/src/components/ArtworkScreen.tsx
+++ client/src/components/ArtworkScreen.tsx
@@ -1,26 +1,38 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { ICategory } from '../atoms';
+import {
+  lateDescription,
+  midDescription,
+  earlyDescription,
+} from '../assets/description';
+
+export default function ArtworkScreen({ category }: ICategory) {
+  let navigate = useNavigate();
+
+  const handleClick = (category: ICategory) => {
+    navigate(`/${category}`);
+  };
+
+  return (
+    <Container>
+      <button onClick={() => handleClick(category as any)}>
+        To gallery component
+      </button>
+      {category === 'late' && <Description>{lateDescription}</Description>}
+      {category === 'mid' && <Description>{midDescription}</Description>}
+      {category === 'early' && <Description>{earlyDescription}</Description>}
+    </Container>
+  );
+}
 
 const Container = styled.div`
   border: 1px solid skyblue;
   height: 100vh;
 `;
 
-export default function ArtworkScreen({ category }: ICategory) {
-  let navigate = useNavigate();
-
-  const handleClick = (category: ICategory) => {
-    console.log('category', category);
-    navigate(`/${category}`);
-  };
-
-  return (
-    <Container>
-      Screen2
-      <button onClick={() => handleClick(category as any)}>
-        To gallery component
-      </button>
-    </Container>
-  );
-}
+const Description = styled.div`
+  border: 1px solid blue;
+  /* height: 100px; */
+  width: 500px;
+`;
