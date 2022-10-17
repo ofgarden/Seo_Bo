d69d9978f09e5bb6de510a84f2fcf813dcf34d93
jungwon <gardenlike.p@gmail.com>
2022-10-17 17:42:11 +0200

    style: update style

--- client/src/components/InfoScreen.tsx
+++ client/src/components/InfoScreen.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
+import endGif from '../assets/end.gif';
 
 const Container = styled.div`
   border: 1px solid skyblue;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   scroll-snap-align: start;
 `;
 
 export default function InfoScreen() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <button>To Exhibition</button>
+    </Container>
+  );
 }
