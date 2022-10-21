3454b21263e44960d214b5f7c345737cdec7ae03
jungwon <gardenlike.p@gmail.com>
2022-10-21 18:27:08 +0200

    style: style infoscreen

--- client/src/components/InfoScreen.tsx
+++ client/src/components/InfoScreen.tsx
@@ -9,6 +9,23 @@ const Container = styled.div`
   scroll-snap-align: start;
 `;
 
+const Button = styled.button`
+  position: absolute;
+  margin: 100px;
+`;
+
+const TransparentCover = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+`;
+
+const BackgroudGif = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
 export default function InfoScreen() {
   const navigate = useNavigate();
 
@@ -18,7 +35,9 @@ export default function InfoScreen() {
 
   return (
     <Container>
-      <button onClick={handleClick}>To Exhibition</button>
+      <TransparentCover />
+      <Button onClick={handleClick}>To Exhibition</Button>
+      <BackgroudGif src={endGif}></BackgroudGif>
     </Container>
   );
 }
