eed9d019da1a77b5a8a4adc950a2ecf83ff0d313
jungwon <gardenlike.p@gmail.com>
2022-10-24 18:22:08 +0200

    ops: change url

--- client/src/ApiService.ts
+++ client/src/ApiService.ts
@@ -1,4 +1,5 @@
-const BASE_URL = `http://localhost:3000`;
+// const BASE_URL = `http://localhost:3000`;
+const BASE_URL = `https://seobo-deploy-server.fly.dev`;
 
 export function fetchArtworks() {
   return fetch(`${BASE_URL}/artworks`).then((response) => response.json())
