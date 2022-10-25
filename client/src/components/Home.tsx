efed9f0d5826d71d0bcc686174eafb545b765469
jungwon <gardenlike.p@gmail.com>
2022-10-25 18:38:06 +0200

    style: lint

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,15 +1,21 @@
+import styled from 'styled-components';
 import MainScreen from './MainScreen';
 import ArtworkScreen from './ArtworkScreen';
 import InfoScreen from './InfoScreen';
 
+const Container = styled.div`
+  scroll-snap-align: start;
+  font-family: poppins;
+`;
+
 export default function Home() {
   return (
-    <div style={{ scrollSnapAlign: 'start', fontFamily: 'poppins' }}>
+    <Container>
       <MainScreen />
       <ArtworkScreen category={'late'} />
       <ArtworkScreen category={'mid'} />
       <ArtworkScreen category={'early'} />
       <InfoScreen />
-    </div>
+    </Container>
   );
 }
