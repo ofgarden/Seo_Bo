dfd25b4b3c8d6c5ff57e8a04fd9acaa6a3756daf
jungwon <gardenlike.p@gmail.com>
2022-10-25 17:56:23 +0200

    refactor: implement each artworks components

--- client/src/components/ArtworkScreen.tsx
+++ client/src/components/ArtworkScreen.tsx
@@ -4,14 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 import { ICategory } from '../atoms';
-import { midDescription, earlyDescription } from '../assets/description';
+import { earlyDescription } from '../assets/description';
 import LateArtworks from './LateArtworks';
+import MidArtworks from './MidArtworks';
+import EarlyArtworks from './EarlyArtworks';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  scroll-snap-align: start;
+`;
 
 const ToGalleryWrapper = styled(motion.div)`
   position: absolute;
   display: flex;
-  top: 15px;
-  right: 30px;
+  top: 1%;
+  right: 1%;
   gap: 5px;
   align-items: center;
   cursor: pointer;
@@ -19,25 +28,12 @@ const ToGalleryWrapper = styled(motion.div)`
 `;
 
 const ToGalleryText = styled(motion.div)`
-  font-size: 15px;
+  font-size: 13px;
   font-weight: 400;
 `;
 
 const MotionToGalleryButton = styled(motion.div)`
   height: 18px;
-`;
-
-const BackgroudImg = styled.img`
-  height: 100%;
-  width: 100%;
-  object-fit: fill;
-`;
-
-const TransparentCover = styled.div`
-  background-color: rgba(255, 255, 255, 0.3);
-  width: 100%;
-  height: 100vh;
-  position: absolute;
 `;
 
 export default function ArtworkScreen({ category }: ICategory) {
@@ -49,16 +45,9 @@ export default function ArtworkScreen({ category }: ICategory) {
 
   return (
     <Container>
-      <TransparentCover />
-      {category === 'late' && (
-        <>
-          <LateArtworks />
-          {/* <Description>{lateDescription}</Description>
-          <BackgroudImg src={lateBackground}></BackgroudImg> */}
-        </>
-      )}
-      {category === 'mid' && <Description>{midDescription}</Description>}
-      {category === 'early' && <Description>{earlyDescription}</Description>}
+      {category === 'late' && <LateArtworks />}
+      {category === 'mid' && <MidArtworks />}
+      {category === 'early' && <EarlyArtworks />}
       <ToGalleryWrapper
         whileHover={{ scale: [null, 1.1, 1] }}
         onClick={() => handleClick(category as any)}
@@ -74,21 +63,3 @@ export default function ArtworkScreen({ category }: ICategory) {
     </Container>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  position: relative;
-  scroll-snap-align: start;
-`;
-
-const Description = styled.div`
-  padding: 20px;
-  line-height: 1.6;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 20px;
-  width: 50%;
-  position: absolute;
-  text-shadow: 1px 2px 2px rgba(0, 0, 0, 0.2);
-  text-align: justify;
-`;
